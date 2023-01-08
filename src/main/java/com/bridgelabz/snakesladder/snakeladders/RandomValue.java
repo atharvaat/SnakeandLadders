252a39732c13e47e9754b7d0c83bb98f17489527
atharva <atharvatodankar.at@gmail.com>
2023-01-08 21:46:43 +0530

    Add: Program to add Random Value

---
+++ src/main/java/com/bridgelabz/snakesladder/snakeladders/RandomValue.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.snakesladder.snakeladders;
+import java.util.Random;
+
+public class RandomValue {
+
+	public static void main(String[] args) {
+
+        Random rand = new Random();
+        int diceValue = rand.nextInt(6)+1;
+        System.out.println("Dice Value is " +diceValue);
+	}
+
+}
