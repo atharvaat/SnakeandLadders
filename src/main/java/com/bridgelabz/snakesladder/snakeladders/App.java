8697273926e071b0d33275a7f9bd0154067b4da5
atharva <atharvatodankar.at@gmail.com>
2023-01-08 21:31:47 +0530

    Add;welcome message for snake ladder program

---
+++ src/main/java/com/bridgelabz/snakesladder/snakeladders/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.snakesladder.snakeladders;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Snakes and Ladders" );
+    }
+}
